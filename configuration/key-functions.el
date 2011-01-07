3659355640a04242ab9e39a1d990d3f542b3d2ce
void <git.email.address@googlemail.com>
2011-01-07 18:59:14 +0100

    Lispified the indentation

--- configuration/key-functions.el
+++ configuration/key-functions.el
@@ -31,40 +31,26 @@
 
 (defun custom-home ()
   (interactive)
-  (let
-      ((line
-        (buffer-substring-no-properties
-         (line-beginning-position) (line-end-position)))
+  (let ((line (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
        (last-whitespace-offset 0)
        (perform-iteration t))
-    (while
-        (and
-         perform-iteration
-         (< last-whitespace-offset (length line)))
-      (if
-          (is-whitespace (aref line last-whitespace-offset))
+    (while (and perform-iteration (< last-whitespace-offset (length line)))
+      (if (is-whitespace (aref line last-whitespace-offset))
           (incf last-whitespace-offset)
         (setq perform-iteration nil)))
-    (if
-        (> (point) (+ (line-beginning-position) last-whitespace-offset))
+    (if (> (point) (+ (line-beginning-position) last-whitespace-offset))
         (back-to-indentation)
       (move-beginning-of-line nil))))
 
 (defun custom-backspace ()
   (interactive)
-  (let*
-      ((line
-        (buffer-substring-no-properties
-         (line-beginning-position) (point)))
-       (string-index (length line))
-       (whitespace-count 0)
-       (perform-iteration t))
-    (while
-        (and perform-iteration (> string-index 0))
+  (let* ((line (buffer-substring-no-properties (line-beginning-position) (point)))
+         (string-index (length line))
+         (whitespace-count 0)
+         (perform-iteration t))
+    (while (and perform-iteration (> string-index 0))
       (decf string-index)
-      (cond
-       ((is-whitespace (aref line string-index))
-        (incf whitespace-count 1))
-       (t (setq perform-iteration nil))))
-    (backward-delete-char-untabify
-     (max whitespace-count 1))))
+      (cond ((is-whitespace (aref line string-index))
+             (incf whitespace-count 1))
+            (t (setq perform-iteration nil))))
+    (backward-delete-char-untabify (max whitespace-count 1))))
