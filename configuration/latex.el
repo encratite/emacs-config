cb142e31619ef8b82b2f9fd06a4751f2f0045dbf
void <git.email.address@googlemail.com>
2011-01-25 18:46:29 +0100

    Improved the LaTeX itemizer

--- configuration/latex.el
+++ configuration/latex.el
@@ -9,12 +9,19 @@
 (defun setup-latex-mode ()
   (add-hook 'latex-mode-hook 'latex-mode-hooks))
 
+(defun itemize-line (line)
+  (let* ((prefix "\\item ")
+         (line-prefix (subseq line 0 (min (length line) (length prefix)))))
+    (if (string= prefix line-prefix)
+        line
+      (concat prefix line))))
+
 (defun itemize-region ()
   (interactive)
   (if mark-active
       (let* ((selected-text (buffer-substring-no-properties (region-beginning) (region-end)))
              (lines (split-string selected-text "\n"))
-             (itemised-lines (mapcar (lambda (x) (format "\\item %s" x)) lines))
+             (itemised-lines (mapcar 'itemize-line lines))
              (new-inner-text (mapconcat 'identity itemised-lines "\n"))
              (replacement (format "\\begin{itemize}\n%s\n\\end{itemize}" new-inner-text)))
         (kill-region (region-beginning) (region-end))
