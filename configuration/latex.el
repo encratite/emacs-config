7adff08680a393cfc35b63cc304af215aa721a54
void <git.email.address@googlemail.com>
2011-01-31 05:28:46 +0100

    Added "enumerate" to the LaTeX mode

--- configuration/latex.el
+++ configuration/latex.el
@@ -4,7 +4,12 @@
 
 (defun latex-mode-hooks ()
   (local-set-key (kbd "RET") 'latex-mode-enter)
-  (local-set-key (kbd "C-i") 'itemize-region))
+  (local-set-key (kbd "C-i") (lambda ()
+                               (interactive)
+                               (itemise-region "itemize")))
+  (local-set-key (kbd "C-o") (lambda ()
+                               (interactive)
+                               (itemise-region "enumerate"))))
 
 (defun setup-latex-mode ()
   (add-hook 'latex-mode-hook 'latex-mode-hooks))
@@ -16,14 +21,14 @@
         line
       (concat prefix line))))
 
-(defun itemize-region ()
+(defun itemise-region (string)
   (interactive)
   (if mark-active
       (let* ((selected-text (buffer-substring-no-properties (region-beginning) (region-end)))
              (lines (split-string selected-text "\n"))
              (itemised-lines (mapcar 'itemize-line lines))
              (new-inner-text (mapconcat 'identity itemised-lines "\n"))
-             (replacement (format "\\begin{itemize}\n%s\n\\end{itemize}" new-inner-text)))
+             (replacement (format "\\begin{%s}\n%s\n\\end{%s}" string new-inner-text string)))
         (kill-region (region-beginning) (region-end))
         (insert replacement))
     (message "The mark is currently not active.")))
